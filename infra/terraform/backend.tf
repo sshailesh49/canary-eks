f3ede13ea84c5a3ebf8bf71927cf4181bb484ce5
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-07-04 23:26:49 +0530

    Delete infra/terraform/backend.tf

--- infra/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-pankaj"     # ✅ S3 bucket name
-    key            = "env/dev/terraform.tfstate"  # 🗂️ file path in S3
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"       # 🔐 locking for concurrency
-    encrypt        = true
-  }
-}
