248aef3f1daf85f34d957a3e38ea7388815db8e2
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-07-04 23:22:10 +0530

    Update backend.tf

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-eks-terraform-state-sshailesh49"     # ✅ S3 bucket name
+    bucket         = "my-pankaj"     # ✅ S3 bucket name
     key            = "env/dev/terraform.tfstate"  # 🗂️ file path in S3
     region         = "us-west-2"
     dynamodb_table = "terraform-lock-table"       # 🔐 locking for concurrency
