8d3e70545bdbee1605dc6029dadf561c857b0693
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-07-05 14:29:33 +0530

    Create helm-prometheus.tf

---
+++ infra/terraform/helm-prometheus.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "prometheus" {
+  name             = "prometheus"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  create_namespace = true
+
+  values = [file("${path.module}/values-prometheus.yaml")]
+}
